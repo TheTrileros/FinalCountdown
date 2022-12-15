20bbf443c9c515bfe4fa553bb5938afc2b13cbc5
GARS00 <guillermoandres.rs@gmail.com>
2022-12-15 11:44:39 +0100

    Agregar java al footer

---
+++ js/javaScript.js
@@ -0,0 +1,9 @@
+clickfooter=document.getElementById("PiePagina");
+clickfooter.addEventListener("click", function(){abrirfooter()});
+
+function abrirfooter(){
+    let capafooter=document.getElementById("footer");
+    if(capafooter.classList.contains("invisible")){
+        capafooter.classList.remove("invisible");
+    }
+    else capafooter.classList.add("invisible");}
