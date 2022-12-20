5fa04acb0ccfbe023b9e15d25a79760ca9cb6c38
Kevin Panjón <kevinpanjonjara@gmail.com>
2022-12-20 08:44:45 +0100

    Cantidad actualizada

--- js/javaScript.js
+++ js/javaScript.js
@@ -59,13 +59,15 @@ imag.src="/imagenes/carrito-de-compras.png";*/
 var inicio = 1; // Se inicia una variable en 1.
 
 function aumentar(){ // Se crean la funcion y se agrega al evento onclick en en la etiqueta button con id aumentar.
-
 var cantidad = document.getElementById('cantidad').value = ++inicio; // Se obtiene el valor del input, y se incrementa en 1 el valor que tenga.
 }
 
-function disminuir(){ // Se crean la funcion y se agrega al evento onclick en en la etiqueta button con id disminuir.
-
+function disminuir(number){ // Se crean la funcion y se agrega al evento onclick en en la etiqueta button con id disminuir.
 var cantidad = document.getElementById('cantidad').value = --inicio; // Se obtiene el valor del input, y se decrementa en 1 el valor que tenga.
+if(number === 0){
+    result = "0"
+    return result
+}
 }
 
 /* BOTÓN AÑADIR AL CARRITO */
